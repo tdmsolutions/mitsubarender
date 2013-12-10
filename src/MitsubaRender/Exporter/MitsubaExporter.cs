69e3ecab36ee345e1459aa99a37359c0b3eb2665
Guillermo-Sotomayor-TDM <guillermo@tdmsolutions.com>
2013-12-10 15:41:09 +0100

    - License terms

--- src/MitsubaRender/Exporter/MitsubaExporter.cs
+++ src/MitsubaRender/Exporter/MitsubaExporter.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+ * This file is part of MitsubaRenderPlugin project.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MitsubaRenderPlugin.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2014 TDM Solutions SL
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
@@ -81,6 +98,12 @@ namespace MitsubaRender
 
 				/* Create a sensor for the current view */
 				ExportSensor(docRoot, doc.Views.ActiveView.ActiveViewport);
+
+			    if (doc.RenderEnvironments.Count > 0)
+			    {
+			        RenderEnvironment env = doc.RenderEnvironments[0];
+                    
+			    }
 
 				/* Write the XML scene document */
 			    //WriteSceneDoc(sceneFile, stopwatch, m_settings);
