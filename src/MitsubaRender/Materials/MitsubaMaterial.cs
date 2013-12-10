e71a6d7f072f1327cbb290700cc003aa59087bf7
Guillermo-Sotomayor-TDM <guillermo@tdmsolutions.com>
2013-12-10 15:02:41 +0100

    - Adding some comments

--- src/MitsubaRender/Materials/MitsubaMaterial.cs
+++ src/MitsubaRender/Materials/MitsubaMaterial.cs
@@ -3,8 +3,17 @@ using Rhino.Render;
 
 namespace MitsubaRender.MitsubaMaterials
 {
+    /// <summary>
+    /// Parent material for all Mitsuba materials
+    /// </summary>
     public abstract class MitsubaMaterial:RenderMaterial
     {
+        /// <summary>
+        /// Generate the xml material for mitsuba render
+        /// </summary>
+        /// <param name="doc">Xml document</param>
+        /// <param name="destFile"></param>
+        /// <returns>the material xml element</returns>
         public abstract XmlElement GetMitsubaMaterialXml(XmlDocument doc, string destFile);
 
         public abstract string GetIdMaterial();
